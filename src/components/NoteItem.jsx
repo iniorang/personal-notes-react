5feb58418bcf1863dba9e8738732e124bf4f1955
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-12-02 23:06:01 +0700

    Add Button

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import NoteItemBody from "./NoteItemBody";
-
+import DeleteButton from "./DeleteButton";
+import ArchiveButton from "./ArchiveButton";
 
 function NoteItem({ title, createdAt, body }) {
   return (
     <div className="note-item">
-      <NoteItemBody title={title} body={body} createdAt={createdAt}  />
+      <NoteItemBody title={title} body={body} createdAt={createdAt} />
+      <div className="note-item__action">
+        <DeleteButton />
+        <ArchiveButton />
+      </div>
     </div>
   );
 }
